c7d4dac9c665790e2a60816e28373ec199ab6eec
zhxkeith <zhxkeith@gmail.com>
2020-07-09 11:02:52 +0800

    springboot

--- src/main/java/com/zhx/springboot/web/TestController.java
+++ src/main/java/com/zhx/springboot/web/TestController.java
@@ -11,7 +11,7 @@ public class TestController {
    @RequestMapping("/test")
     public String test(Model m){
        String htmlContent = "<p style='color:red'> 红色文字</p>";
-       Poduct currentProduct =new Poduct(5,"product e", 200);
+       Poduct currentProduct =new Poduct(5,"product 5", 200);
        m.addAttribute("htmlContent", htmlContent);
        m.addAttribute("currentProduct", currentProduct);
        return "test";
